31ab9b387a1b64096c70ec1d15da22ab068f0827
Yuta123456 <yuuta09090530@icloud.com>
2022-07-17 15:38:23 +0900

    util

---
+++ spotify/util.py
@@ -0,0 +1,42 @@
+import datetime
+from operator import ge
+from unittest import result
+
+from requests import get
+from sympy import limit
+from spotify import spotify
+
+
+def debut_day_of_artist(artist_id):
+    albums = []
+    # ごめんなさい。
+    result_album = True
+    offset = 0
+    while result_album:
+        result_album = spotify.artist_albums(
+            artist_id, limit=50, offset=offset)['items']
+        offset += 50
+        albums += result_album
+    albums = filter(lambda x: int(
+        x['release_date'].split('-')[0]) > 1900, albums)
+    debut_year_album = min(albums, key=lambda x: x['release_date'])
+    release_date = debut_year_album['release_date']
+    if len(release_date.split('-')) < 3:
+        release_date += "-01" * (3 - len(release_date.split('-')))
+    # print(release_date, debut_year_album)
+    return release_date
+
+
+def get_popularity_based_on_date(artist_id):
+    artist = spotify.artist(artist_id)
+    popularity = artist['popularity']
+    debut_day = debut_day_of_artist(artist_id)
+    debut_year = datetime.datetime.strptime(
+        debut_day, "%Y-%m-%d").strftime("%Y")
+    this_year = datetime.date.today().strftime("%Y")
+    sub = int(this_year) - int(debut_year)
+    print(this_year, debut_day)
+
+
+# get_popularity_based_on_date("3cbd5GWGOknxmFAe77MDbk")
+# debut_day_of_artist("2DaxqgrOhkeH0fpeiQq2f4")
